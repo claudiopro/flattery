f46e9572232285399ed4ce040fc52a179c52c0fe
claudiopro <claudio.procida@gmail.com>
2016-12-13 16:10:33 +0100

    Converts the project to Mocha ☕️  and Chai 🍵

--- test/test.js
+++ test/test.js
@@ -1,36 +1,37 @@
-var flattery = require('..');
+var flattery = require('..'),
+  expect = require('chai').expect;
 
 describe('the flattery util', function() {
   it('reduces a nested array to a flat list of integers', function() {
-    expect(flattery([1, [2, 3], [4]])).toEqual([1, 2, 3, 4]);
-    expect(flattery([[1, 2], 3, [4]])).toEqual([1, 2, 3, 4]);
-    expect(flattery([1, [2, [3, 4]]])).toEqual([1, 2, 3, 4]);
-    expect(flattery([1, [2, [3]], 4])).toEqual([1, 2, 3, 4]);
-    expect(flattery([[1, 2, [3]], 4])).toEqual([1,2,3,4]);
+    expect(flattery([1, [2, 3], [4]])).to.eql([1, 2, 3, 4]);
+    expect(flattery([[1, 2], 3, [4]])).to.eql([1, 2, 3, 4]);
+    expect(flattery([1, [2, [3, 4]]])).to.eql([1, 2, 3, 4]);
+    expect(flattery([1, [2, [3]], 4])).to.eql([1, 2, 3, 4]);
+    expect(flattery([[1, 2, [3]], 4])).to.eql([1,2,3,4]);
   });
 
   it('does not alter a flat array', function() {
-    expect(flattery([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+    expect(flattery([1, 2, 3, 4])).to.eql([1, 2, 3, 4]);
   });
 
   it('returns an empty array when passed in an empty array', function() {
-    expect(flattery([])).toEqual([]);
+    expect(flattery([])).to.eql([]);
   });
 
   it('throws when called on a scalar value', function() {
-    expect(function(){flattery(1)}).toThrow();
+    expect(function(){flattery(1)}).to.throw;
   });
 
   it('does not throw when called on an array', function() {
-    expect(function(){flattery([1])}).not.toThrow();
-    expect(function(){flattery([1, 2])}).not.toThrow();
-    expect(function(){flattery([1, [2]])}).not.toThrow();
-    expect(function(){flattery([1, [2, 3]])}).not.toThrow();
+    expect(function(){flattery([1])}).to.not.throw();
+    expect(function(){flattery([1, 2])}).to.not.throw();
+    expect(function(){flattery([1, [2]])}).to.not.throw();
+    expect(function(){flattery([1, [2, 3]])}).to.not.throw();
   });
 
   it('works well on strings', function() {
-    expect(flattery([23, 'foo', [34, 1], [['bar', [12]]]])).toEqual([23, 'foo', 34, 1, 'bar', 12]);
-    expect(flattery([23, ['foo', [34, 1]], [['bar', [12]]]])).toEqual([23, 'foo', 34, 1, 'bar', 12]);
-    expect(flattery([[23], ['foo'], [34, [1]], [['bar', [12]]]])).toEqual([23, 'foo', 34, 1, 'bar', 12]);
+    expect(flattery([23, 'foo', [34, 1], [['bar', [12]]]])).to.eql([23, 'foo', 34, 1, 'bar', 12]);
+    expect(flattery([23, ['foo', [34, 1]], [['bar', [12]]]])).to.eql([23, 'foo', 34, 1, 'bar', 12]);
+    expect(flattery([[23], ['foo'], [34, [1]], [['bar', [12]]]])).to.eql([23, 'foo', 34, 1, 'bar', 12]);
   });
 });
